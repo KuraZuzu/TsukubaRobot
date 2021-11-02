716d65513aafbad92cc4ba67b803154e2deb93d0
zuzukun <7bda2106@mail.u-tokai.ac.jp>
2021-11-02 18:16:40 +0900

    これでも速度計測が出来ないので、まずい

--- Src/EFPMachineControl/Test.h
+++ Src/EFPMachineControl/Test.h
@@ -49,6 +49,6 @@ private:
     int32_t _speed; // [mm]/[s]
 };
 
-static Test test_wheel(mslh::WheelControl(mslh::Motor(htim1, TIM_CHANNEL_1, GPIOC, GPIO_PIN_0, false), mslh::Encoder(htim3, 500*6*23, true), 3000, 1), mslh::WheelControl(mslh::Motor(htim1, TIM_CHANNEL_2, GPIOC, GPIO_PIN_3, true), mslh::Encoder(htim4, 500*6*23, false), 3000, 1));
+static Test test_wheel(mslh::WheelControl(mslh::Motor(htim1, TIM_CHANNEL_1, GPIOC, GPIO_PIN_0, false), mslh::Encoder(htim3, 500*6*23*4, true), 300, 1), mslh::WheelControl(mslh::Motor(htim1, TIM_CHANNEL_2, GPIOC, GPIO_PIN_3, true), mslh::Encoder(htim4, 500*6*23*4, false), 300, 1));
 
 #endif //TSUKUBAROBOT_TEST_H
